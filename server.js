39c09611d5a81fce06ad5abe0e15932669a6f539
lewhat <lewhat@live.com>
2016-05-28 12:45:54 -0700

    second initial deploy

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express()
 var path = require('path')
 var bodyParser = require('body-parser')
 var routes = require('./src/server/routes')
+var deployd = require('deployd')
+
 
 var PORT = process.env.PORT || 3000
 
@@ -17,6 +19,39 @@ app.all('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-app.listen(process.env.Port || 3000, function() {
+app.listen(PORT, function() {
     console.log('Server running on ' + PORT)
+})
+
+
+
+
+var server = deployd({
+
+	port: process.env.PORT || 5000,
+	env: 'production',
+	db: {
+		host:'ds019053.mlab.com',
+		port:19053,
+		name:'heroku_3szsjwjc',
+		credentials: {
+			username:'heroku_3szsjwjc',
+			password:'g7rorn4v63ip5v15i1qq49d3fs'
+		}
+	}
+
+})
+
+server.listen()
+
+server.on('listening', function(){
+	console.log('Server is listening')
+})
+
+
+server.on('error', function(err){
+	console.log(err)
+	process.nextTick(function(){
+		process.exit()
+	})
 })
--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todos')
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todo-app')
 
 var Todo = mongoose.model('Todo', {
     task: String,
