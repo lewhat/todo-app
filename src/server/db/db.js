beaa3878886527151a1cd314f853f18528294a9a
lewhat <lewhat@live.com>
2016-08-04 13:06:20 -0700

    cleaned it a little

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose')
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todos')
+var configValues = require('./config')
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds139715.mlab.com:39715/lewhatsnodetodo')
 
 var Todo = mongoose.model('Todo', {
     task: String,
